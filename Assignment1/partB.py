7a15ef8c8f454e035687574af4c0916df99e3f26
Jimmy Jin <jimmyj2@uci.edu>
2023-04-13 14:44:00 -0700

    coding intersect function for partB

--- Assignment1/partB.py
+++ Assignment1/partB.py
@@ -1,8 +1,21 @@
 import sys
 import partA
 
+def intersection(fileA,fileB):
+    fileA_tokens = partA.tokenize(fileA)
+    fileB_tokens = partA.tokenize(fileB)
+    dictA = partA.computeWordFrequencies(fileA_tokens)
+    dictB = partA.computeWordFrequencies(fileB_tokens)
+
+    setA = set(dictA.keys())
+    setB = set(dictB.keys())
+
+    intersect = setA.intersection(setB)
+
 
 
 def main():
-    filePath = sys.argv[1]
-    partA.tokenize(filePath)
+    files  = sys.argv
+    fileA = files[1]
+    fileB = files[2]
+    intersection(fileA,fileB)
